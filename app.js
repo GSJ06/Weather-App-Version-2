3919bb6add4b0b282610d7e058b8a607867da05c
Goutam Joshi <joshigoutam04@gmail.com>
2023-09-30 22:47:19 +0530

    exception handled

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Constants
-const KELVIN = 273;
+const KELVIN = 273; // Kelvin temperature offset
 const API_KEY = ""; // Add your actual API key
 
 // DOM Elements
@@ -19,7 +19,7 @@ const searchButton = document.getElementById("searchButton");
 const weather = {
   temperature: {
     value: undefined,
-    unit: "C", // Default unit
+    unit: "C", // Default temperature unit
   },
   description: "",
   iconId: "",
@@ -27,20 +27,21 @@ const weather = {
   country: "",
   temp_max: {
     value: undefined,
-    unit: "C", // Default unit
+    unit: "C", // Default temperature unit
   },
-  temp_min:{
+  temp_min: {
     value: undefined,
-    unit: "C", // Default unit
+    unit: "C", // Default temperature unit
   },
-  pressure:"",
-  humidity:"",
+  pressure: "",
+  humidity: "",
+  wind: "",
 };
 
 // Event listeners
 searchButton.addEventListener("click", () => {
-  notificationElement.style.display = "none";
-  notificationElement.innerHTML = '';
+  notificationElement.style.display = "none"; // Hide any previous error messages
+  notificationElement.innerHTML = ""; // Clear the error message text
   const location = locationInput.value.trim();
   if (!location) {
     alert("Please enter a location.");
@@ -50,45 +51,59 @@ searchButton.addEventListener("click", () => {
 });
 
 tempElement.addEventListener("click", () => {
-  toggleTemperatureUnit();
+  toggleTemperatureUnit(); // Toggle temperature unit when clicked
 });
 
-// Geolocation support check
+// Check if geolocation is supported by the browser
 if ('geolocation' in navigator) {
   navigator.geolocation.getCurrentPosition(setPosition, showError);
 } else {
   displayError("Browser doesn't support geolocation.");
 }
 
-// Functions (refactored)
+// Functions
+
+// Set the user's location based on geolocation data
 function setPosition(position) {
   const { latitude, longitude } = position.coords;
   getWeatherByCoordinates(latitude, longitude);
 }
 
+// Handle geolocation error
 function showError(error) {
   displayError(error.message);
 }
 
+// Display an error message on the frontend
 function displayError(message) {
   notificationElement.style.display = "block";
   notificationElement.innerHTML = `<p>${message}</p>`;
+  clearWeatherData(); // Clear weather data on error
 }
 
+// Fetch weather data by city name
 function getWeather(location) {
   const api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
   fetchWeatherData(api);
 }
 
+// Fetch weather data by coordinates
 function getWeatherByCoordinates(latitude, longitude) {
   const api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
   fetchWeatherData(api);
 }
 
+// Fetch weather data from the API
 function fetchWeatherData(api) {
   fetch(api)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("City not found"); // Handle API error
+      }
+      return response.json();
+    })
     .then((data) => {
+      // Parse API data and store it in the 'weather' object
       weather.temperature.value = Math.floor(data.main.temp - KELVIN);
       weather.temp_min.value = Math.floor(data.main.temp_min - KELVIN);
       weather.temp_max.value = Math.floor(data.main.temp_max - KELVIN);
@@ -101,14 +116,20 @@ function fetchWeatherData(api) {
       weather.country = data.sys.country;
     })
     .then(() => {
-      displayWeather();
+      displayWeather(); // Display weather data on success
     })
     .catch((error) => {
-      displayError("An error occurred while fetching weather data.");
-      console.error(error);
+      if (error.message === "City not found") {
+        displayError("City not found. Please enter a valid city name.");
+      } else {
+        displayError("An error occurred while fetching weather data.");
+        console.error(error);
+      }
+      clearWeatherData(); // Clear weather data on error
     });
 }
 
+// Display weather data on the frontend
 function displayWeather() {
   iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
   tempElement.innerHTML = `${weather.temperature.value}°<span>${weather.temperature.unit.toUpperCase()}</span>`;
@@ -120,19 +141,33 @@ function displayWeather() {
   locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
 
+// Toggle temperature unit between Celsius and Fahrenheit
 function toggleTemperatureUnit() {
   if (weather.temperature.value === undefined) return;
 
-  if (weather.temperature.unit === "celsius") {
+  if (weather.temperature.unit === "C") {
     const fahrenheit = celsiusToFahrenheit(weather.temperature.value);
     tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
-    weather.temperature.unit = "fahrenheit";
+    weather.temperature.unit = "F";
   } else {
     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    weather.temperature.unit = "celsius";
+    weather.temperature.unit = "C";
   }
 }
 
+// Convert temperature from Celsius to Fahrenheit
 function celsiusToFahrenheit(temperature) {
   return (temperature * 9/5) + 32;
+}
+
+// Clear weather data on error
+function clearWeatherData() {
+  iconElement.innerHTML = "";
+  tempElement.innerHTML = "";
+  tempMinElement.innerHTML = "";
+  tempMaxElement.innerHTML = "";
+  humidityElement.innerHTML = "";
+  windspeedElement.innerHTML = "";
+  descElement.innerHTML = "";
+  locationElement.innerHTML = "";
 }
